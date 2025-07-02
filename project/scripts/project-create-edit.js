66b9fd676bb30c6481c38a7dfc327da43b79ca75
fabianoGDB <doctorfabi.f@gmail.com>
2025-07-02 03:57:06 -0300

    A017: creating the post project method

--- project/scripts/project-create-edit.js
+++ project/scripts/project-create-edit.js
@@ -1,5 +1,5 @@
 window.onload = function(){
-    const screenType = 'edit';
+    const screenType = 'create';
     let mainTitle = document.querySelector('#main-title');
 
     if(screenType == 'create')
@@ -8,9 +8,39 @@ window.onload = function(){
         document.querySelector('#action-button').innerText = "Create";
     }
 
-    if(screenType == 'edit')
-    {
-        mainTitle.innerText = "Edit project!";
-        document.querySelector('#action-button').innerText = "Save";
+
+
+    // if(screenType == 'edit')
+    // {
+    //     mainTitle.innerText = "Edit project!";
+    //     document.querySelector('#action-button').innerText = "Save";
+    // }
+}
+
+
+function Create(){
+
+
+    let payload = {
+        title: document.querySelector("#title").value,
+        description: document.querySelector("#description").value,
+        totalCost: document.querySelector("#totalPrice").value,
+        idClient: "1"
     }
+
+    fetch("https://6864b9585b5d8d03397e05dc.mockapi.io/api/projects", {
+        method: "POST",
+        body: JSON.stringify(payload),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    })
+    .then(response => response.json())
+    .then(response => {
+        console.log(response)
+    })
+    .catch(error => {
+        console.log(error)
+    })
+
 }
