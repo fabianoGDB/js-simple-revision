6d76f1e535fcf7a311451c54a06b14243bf65208
fabianoGDB <doctorfabi.f@gmail.com>
2025-07-03 02:47:40 -0300

    A019: adding to the edit screen the previuew data to be updated

--- project/scripts/list.js
+++ project/scripts/list.js
@@ -3,7 +3,7 @@ window.onload = function(){
     document.querySelector("#name").innerText = localStorage.getItem("user.name");
     document.querySelector("#role").innerText = localStorage.getItem("user.role");
 
-    GetProjects()
+    GetProjects();
 }
 
 function GetProjects(){
--- project/scripts/project-create-edit.js
+++ project/scripts/project-create-edit.js
@@ -4,6 +4,19 @@ const screenType = params.id ? 'edit' : 'create';
 
 window.onload = function(){
     SetScreenType();
+    FillInputs();
+}
+
+function FillInputs(){
+    if(screenType === 'edit'){
+        fetch(`https://6864b9585b5d8d03397e05dc.mockapi.io/api/projects/${params.id}`)
+        .then(response => response.json())
+        .then(project => {
+            document.querySelector("#title").value = project.title;
+            document.querySelector("#totalPrice").value = project.totalCost;
+            document.querySelector("#description").value = project.description;
+        })
+    }
 }
 
 function SetScreenType(){
